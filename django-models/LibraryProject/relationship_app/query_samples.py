5a45b4da3caf641dafa01df267582526dad41c8c
akramiga <akramlubega73@gmail.com>
2025-02-21 11:42:35 +0300

    added data to quesries.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,3 +4,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 from relationship_app.models import Library, Librarian, Author, Book
 
+def list_books():
+    return Library.objects.get(name = 'library')
+
+def books_by_author(name):
+    return Book.objects.filter(author = name)
+
