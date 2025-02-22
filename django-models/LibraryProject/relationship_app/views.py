4fab92a88305d6b8cc71fb87b4853859c04e705c
akramiga <akramlubega73@gmail.com>
2025-02-22 18:48:08 +0300

    added detailview

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Library, Book
 from django.views import View
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView, ListView
 # Create your views here.
 #function based view
 def list_books(request):
