e2d3f00973f099490ee232e752e604fd627f01a5
akramiga <akramlubega73@gmail.com>
2025-02-22 18:44:21 +0300

    added detailview

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Library, Book
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 # Create your views here.
 #function based view
 def list_books(request):
@@ -19,3 +19,13 @@ class LibraryView(ListView):
     context_object_name = 'libraries'
     def get_queryset(self):
         return self.model.objects.all()   
+    
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"  # Ensure this template exists
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["books"] = self.object.book_set.all()  # Fetch all books in this library
+        return context
