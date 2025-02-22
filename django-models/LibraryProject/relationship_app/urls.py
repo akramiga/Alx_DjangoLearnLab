ab7acccced98fb5dee1b6eb171c74a009c1898ec
akramiga <akramlubega73@gmail.com>
2025-02-22 18:53:19 +0300

    added detailview

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import list_books
 urlpatterns = [
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('library/', views.LibraryView.as_view(), name='library'),
