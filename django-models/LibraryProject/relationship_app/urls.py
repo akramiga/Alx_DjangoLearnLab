14322e2339c545cd26f6107e30d4ac764dd4da28
akramiga <akramlubega73@gmail.com>
2025-03-01 15:03:13 +0300

    corrections to urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     path('librarian_dashboard/',librarian_view, name='librarian_dashboard'),
     path('member_dashboard/', member_view, name='member_dashboard'),
 
-    path('add/', views.add_book, name='add_book'),
-    path('edit/<int:book_id>/', views.edit_book, name='edit_book'),
-    path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/', views.edit_book, name='edit_book'),
+    path('delete_book', views.delete_book, name='delete_book'),
   
 ]
