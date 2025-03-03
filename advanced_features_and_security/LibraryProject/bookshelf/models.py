ec8d3b24e9c8d6eb236463df5b8ec3f701220d67
akramiga <akramlubega73@gmail.com>
2025-03-04 00:37:48 +0300

    added custom models to file

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractUser, BaseUserManager
 
 # Create your models here.
 class Book(models.Model):
@@ -8,3 +9,25 @@ class Book(models.Model):
     
     def __str__(self):
         return self.title
+    
+#creating acustom user model and adding custom fields
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(unique= False)
+    profile_photo = models.ImageField(upload_to='images/', null= True)
+    def __str__(self):
+        return self.username
+#custom user manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password = None):
+        if not email:
+            return ValueError('PLEASE ADD EMAIL')
+        user = self.model(email= self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user 
+    def create_superuser(self, email, password = None):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user    
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser, BaseUserManager
+from django.contrib.auth.models import User 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
@@ -59,24 +59,3 @@ def create_user(sender, instance, created, **kwargs):
 def save_user(sender, instance, **kwargs):
     instance.userprofile.save()        
 
-#creating acustom user model and adding custom fields
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(unique= False)
-    profile_photo = models.ImageField(upload_to='images/', null= True)
-    def __str__(self):
-        return self.username
-#custom user manager
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password = None):
-        if not email:
-            return ValueError('PLEASE ADD EMAIL')
-        user = self.model(email= self.normalize_email(email))
-        user.set_password(password)
-        user.save(using=self._db)
-        return user 
-    def create_superuser(self, email, password = None):
-        user = self.create_user(email, password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
