578b1664016baaaff3eda5d801e60857f1adf50a
akramiga <akramlubega73@gmail.com>
2025-03-05 15:12:26 +0300

    new commit

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser, BaseUserManager
-
+from django.contrib.auth.models import Permission, Group
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+    class Meta:
+        permissions = [
+            ('can_create', 'can create'),
+            ('can_view', 'can view'),
+            ('can_edit', 'can edit'),
+            ('can_delete', 'can delete') 
+        ]
     
     def __str__(self):
         return self.title
