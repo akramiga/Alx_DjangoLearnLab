2efcdda0f2acc852d13ae9b0af5dd37e03a993ea
akramiga <akramlubega73@gmail.com>
2025-03-08 13:41:13 +0300

    updated variable name in book model

--- api_project/api_project/api/models.py
+++ api_project/api_project/api/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    aouthor = models.CharField(max_length= 200)
+    author = models.CharField(max_length= 200)
