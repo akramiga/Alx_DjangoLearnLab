5650d842a70f5bc2684affea084c9e5b32485472
akramiga <akramlubega73@gmail.com>
2025-03-15 15:36:35 +0300

    customizing views in views.py

---
+++ advanced_api_project/advanced_api_project/api/urls.py
@@ -0,0 +1,10 @@
+from api import views
+from django.urls import path
+
+urlpatterns = [
+    path('/books/list', views.BookListView.as_view(), name='book_list'),
+    path('/books/<int:pk>/', views.BookDetailView.as_view(), name='book_details'),
+    path('/books/create/', views.BookCreateView.as_view(), name='create_book'),
+    path('/books/update/', views.BookUpdateView.as_view(), name='update_book'),
+    path('/books/delete/', views.BookDeleteView.as_view(), name='delete_book'),
+]
--- advanced_api_project/advanced_api_project/api/views.py
+++ advanced_api_project/advanced_api_project/api/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
-
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.response import Response
 # Create your views here.
+class BookListView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'id'
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer  
+    permission_classes = [IsAuthenticated]  
+    def create(self,serializer):
+        serializer.save()
+    def add_book(self, request, *args, **kwargs):
+        """Customize response for better user feedback"""
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response({
+                "message": "Product added successfully!",
+                "product": serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer  
+    permission_classes = [IsAuthenticated]        
