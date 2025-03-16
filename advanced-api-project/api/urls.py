d7d2f8bbda48406b629ea48eaea0576bca5385fc
akramiga <akramlubega73@gmail.com>
2025-03-16 23:15:28 +0300

    new commit with tests

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,7 +2,7 @@ from api import views
 from django.urls import path
 
 urlpatterns = [
-    path('/books/list', views.BookListView.as_view(), name='book_list'),
+    path('/books/list/', views.BookListView.as_view(), name='book_list'),
     path('/books/<int:pk>/', views.BookDetailView.as_view(), name='book_details'),
     path('/books/create/', views.BookCreateView.as_view(), name='create_book'),
     path('/books/update/', views.BookUpdateView.as_view(), name='update_book'),
