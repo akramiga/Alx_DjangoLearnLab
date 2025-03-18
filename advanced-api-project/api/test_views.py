cac3de437ef58cb254e043bf19ae79ac8f8bba3f
akramiga <akramlubega73@gmail.com>
2025-03-18 11:28:48 +0300

    iplemeting tests

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -11,13 +11,19 @@ class ApiTests(TestCase):
     def test_book_list(self):
         response = self.client.get("/books/list/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data,[])  #logic to be improved
             
     def test_book_create(self):
         response = self.client.post("/books/create/")
         self.assertEqual(response.status_code, status.HTTP_200_OK) 
+        self.assertEqual(response.data,[])  #logic to be improved 
+
     def test_book_update(self):
         response = self.client.put("/books/update/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)   
+        self.assertEqual(response.data,[])  #logic to be improved
+
     def test_book_delete(self):
         response = self.client.delete("/books/delete/")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)     
+        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+        self.assertEqual(response.data,[])  #logic to be improved    
