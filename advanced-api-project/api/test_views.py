e7dba759654a78a299089e71c34167de5fc1b1a6
akramiga <akramlubega73@gmail.com>
2025-03-18 11:23:22 +0300

    iplemeting tests

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from rest_framework import status
+from rest_framework.test import APITestCase
 
 # Create your tests here.
 class ApiTests(TestCase):
@@ -8,12 +9,15 @@ class ApiTests(TestCase):
     def tearDown(self):
         ...    
     def test_book_list(self):
-        response = self.client.get("/list/")
+        response = self.client.get("/books/list/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
             
     def test_book_create(self):
-        ...   
+        response = self.client.post("/books/create/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK) 
     def test_book_update(self):
-        ...   
+        response = self.client.put("/books/update/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)   
     def test_book_delete(self):
-        ...       
+        response = self.client.delete("/books/delete/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)     
