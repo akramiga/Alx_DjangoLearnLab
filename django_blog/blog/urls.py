17349d02b958f8f8ec69df3c26cecf4953f36796
akramiga <akramlubega73@gmail.com>
2025-03-25 12:59:43 +0300

    impelementation of modelform

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,14 +7,14 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('profile/', views.profile, name='profile'),
     path('', views.PostListView.as_view(), name='post_list'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('posts/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
-    path('posts/<int:post_id>/comments/new/', views.add_comment, name='add-comment'),
-    path('posts/<int:post_id>/comments/edit/', views.CommentUpdateView.as_view(), name='edit-comment'),
-    path('posts/<int:post_id>/comments/delete/', views.CommentDeleteView.as_view(), name='delete-comment'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/comments/new/', views.add_comment, name='add-comment'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='update_comment'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),
      #/posts/<int:post_id>/comments/new/
-    
+     #comment/<int:pk>/update/", "post/<int:pk>/comments/new/", "comment/<int:pk>/delete/"
 ]
 
