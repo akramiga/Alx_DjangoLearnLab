d711bea38a43f11a0c51e8b8c9acd03c7d5b65a7
akramiga <akramlubega73@gmail.com>
2025-03-26 15:31:16 +0300

    added serializers and views

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,7 +4,6 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password  # Import hashers
 
-User = get_user_model()
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
@@ -13,12 +12,12 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
 
