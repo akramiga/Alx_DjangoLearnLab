9c0d394b6aa96d0f4610634d95302345278a4a49
akramiga <akramlubega73@gmail.com>
2025-03-26 15:06:29 +0300

    added serializers and views

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from.models import CustomUser
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password  # Import hashers
 
 
@@ -11,12 +12,14 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
 
 
+User = get_user_model()
+
 class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-    password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})  # Added CharField for password2
+    password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password', 'password2', 'bio', 'profile_picture')
         extra_kwargs = {
             'first_name': {'required': False, 'allow_blank': True},
@@ -32,11 +35,11 @@ class RegistrationSerializer(serializers.ModelSerializer):
         return data
 
     def create(self, validated_data):
-        validated_data['password'] = make_password(validated_data.pop('password'))  # Hash password
-        validated_data.pop('password2') # Remove password2
-        user = CustomUser.objects.create(**validated_data)
+        validated_data['password'] = make_password(validated_data.pop('password'))
+        validated_data.pop('password2')
+        user = User.objects.create(**validated_data)
+        Token.objects.create(user=user)  # Create token after user creation
         return user
-
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)  # Or email, depending on your authentication
