acf5825cde2218d28ca4d99c3f391e6aace5dcb3
akramiga <akramlubega73@gmail.com>
2025-03-26 15:02:32 +0300

    added serializers and views

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,6 +3,7 @@ from.models import CustomUser
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.hashers import make_password  # Import hashers
 
+
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
@@ -11,24 +12,29 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    password1 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-    password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})  # Add password2
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})  # Added CharField for password2
 
     class Meta:
-        model = CustomUser  # Use CustomUser model
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2','bio','profile_picture') # Include all relevant fields
-        
+        model = CustomUser
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'password2', 'bio', 'profile_picture')
+        extra_kwargs = {
+            'first_name': {'required': False, 'allow_blank': True},
+            'last_name': {'required': False, 'allow_blank': True},
+            'email': {'required': True},
+            'bio': {'required': False, 'allow_blank': True},
+            'profile_picture': {'required': False}
+        }
 
     def validate(self, data):
-        if data['password1'] != data['password2']:
+        if data['password'] != data['password2']:
             raise serializers.ValidationError("Passwords do not match.")
         return data
 
     def create(self, validated_data):
-        validated_data['password1'] = make_password(validated_data.pop('password1'))  # Hash the password
-        validated_data.pop('password2')
+        validated_data['password'] = make_password(validated_data.pop('password'))  # Hash password
+        validated_data.pop('password2') # Remove password2
         user = CustomUser.objects.create(**validated_data)
-        Token.objects.create(user=user)  # create a token for the user automatically
         return user
 
 
